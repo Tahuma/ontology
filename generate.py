7242e14c17dc02972de43ccfef814e528ac8f18d
Benjamin Schnabel <bsesic@users.noreply.github.com>
2023-09-06 10:46:55 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-with open("judaicalink-ontology.md", "r", encoding="utf-8") as f:
-    with open("judaicalink-ontology.ttl", "w", encoding="utf-8") as out:
+with open("ontology.md", "r", encoding="utf-8") as f:
+    with open("ontology.ttl", "w", encoding="utf-8") as out:
         in_code = False
         for line in f:
             if line.startswith("~~~") or line.startswith("```"):
